8e2f7ff15d52decbc75d0249e26885f526c4aa83
root <root@vm161985.local>
2024-05-14 14:07:18 +0300

    minor stuff

--- wordle_bot.py
+++ wordle_bot.py
@@ -27,6 +27,22 @@ def getBoard(array):
         board+='\n'
     return board
 
+def send_help(chat_id):
+    m='''Your objective is to guess a secret five-letter word in as few guesses as possible. To submit a guess, type any five-letter word and press enter. All of your guesses must be real words. The bot will color-code each letter in your guess to tell you how close it was to the letters in the hidden word.
+A gray or black square means that this letter does not appear in the secret word at all.
+A yellow square means that this letter appears in the secret word, but it’s in the wrong spot within the word.
+A green square means that this letter appears in the secret word, and it’s in exactly the right place.
+You'll have six tries to guess the word.
+Press /start to play.
+___
+Цель игры — как можно быстрее угадать слово из пяти букв. Наберите любое существующее слово из пяти букв и отправьте боту. В ответ он пришлет вам результат вашей попытки. 
+Серый квадрат на месте буквы значит, что ее в слове нет. 
+Желтый — такая буква есть, но на другом месте.
+Зеленый — такая буква есть, и именно здесь.
+На то, чтобы угадать слово, у вас будет шесть попыток.
+Нажмите /start, чтобы сыграть.'''
+    bot.send_message(chat_id,m)    
+
 try:
     conn = mariadb.connect(
         user="wordlerbot",
@@ -45,6 +61,10 @@ cur = conn.cursor()
 
 
 # handlers
+@bot.message_handler(commands=['help'])
+def helper(message):
+    send_help(message.chat.id)
+
 @bot.message_handler(commands=['start', 'go'])
 def start_handler(message):
     #journal.write(message)
@@ -79,6 +99,11 @@ def start_handler(message):
 def askLang(message):
     chat_id = message.chat.id
     text = message.text
+    if (text is None):
+        bot.send_message(chat_id, 'Input must not be empty! Press /start to go on')
+        return
+    if (message.text == '/help'):
+        send_help(chat_id)
     variables[chat_id]['mode'] = 'RUS' if text == 'RUS' else 'ENG'
     with open(dir + '/' + ('eng' if variables[chat_id]['mode'] == 'ENG' else 'rus') + '_fivers.txt',
               encoding="utf-8") as f:
@@ -88,7 +113,7 @@ def askLang(message):
             variables[chat_id]['wordlist_selectable'] = f.readlines()
             variables[chat_id]['word'] = random.choice(variables[chat_id]['wordlist_selectable'])
     journal.write(str(chat_id)+': Word is chosen: ' + variables[chat_id]['word'])
-    msg = bot.send_message(chat_id, '6 tries left' if variables[chat_id]['mode'] == 'ENG' else 'Осталось 6 попыток',
+    msg = bot.send_message(chat_id, '6 tries left. Input your word.' if variables[chat_id]['mode'] == 'ENG' else 'Введите слово. У вас 6 попыток',
                            reply_markup=None)
     bot.register_next_step_handler(msg, guessStep)
 
@@ -96,15 +121,21 @@ def askLang(message):
 def guessStep(message):
     chat_id = message.chat.id
     text = message.text
-    if (len(text)>0):
+    if (text is None):
+        bot.send_message(chat_id, 'Input must not be empty! Press /start to go on')
+        return
+    if (message.text == '/help'):
+        send_help(chat_id)
+    if (text is not None):
         text = text.lower()
     journal.write(str(chat_id)+': Step no ' + str(variables[chat_id]['tries']))
+    journal.write(text)
     # check word
-    if len(text) != 5:
+    if (text is not None) and len(text) != 5:
         msg = bot.send_message(chat_id,
                                'Word must be 5 characters long\n' + str(variables[chat_id]['tries']) + ' tries left' if
                                variables[chat_id][
-                                   'mode'] == 'ENG' else 'Слово должно быть длиной в 5 букв\nОсталось ' + str(
+                                   'mode'] == 'ENG' else 'Слово должно быть длиной в 5 букв\nУ вас ' + str(
                                    variables[chat_id]['tries']) + (' попыток' if variables[chat_id][
                                                                                      'tries'] >= 5 else ' попытки' if
                                variables[chat_id]['tries'] > 1 else ' попытка'), reply_markup=None)
@@ -113,7 +144,7 @@ def guessStep(message):
         journal.write(str(chat_id)+': Candidate: ' + text)
         msg = bot.send_message(chat_id, 'Word must be present in dictionary\n' + str(
             variables[chat_id]['tries']) + ' tries left' if variables[chat_id][
-                                                                'mode'] == 'ENG' else 'Слово должно встречаться в словаре\nОсталось ' + str(
+                                                                'mode'] == 'ENG' else 'Слово должно встречаться в словаре\nУ вас ' + str(
             variables[chat_id]['tries']) + (' попыток' if variables[chat_id]['tries'] >= 5 else ' попытки' if
         variables[chat_id]['tries'] > 1 else ' попытка'), reply_markup=None)
         bot.register_next_step_handler(msg, guessStep)
@@ -149,8 +180,8 @@ def guessStep(message):
             answer = ''
             answer=getBoard([variables[chat_id]['res'][5-variables[chat_id]['tries']]])
             msg = bot.send_message(chat_id, answer + (
-                '\n' + str(variables[chat_id]['tries']) + ' tries left' if variables[chat_id][
-                                                                               'mode'] == 'ENG' else '\nОсталось ' + str(
+                '\n' + str(variables[chat_id]['tries']) + ' tries left. Input your word.' if variables[chat_id][
+                                                                               'mode'] == 'ENG' else '\nВведите слово. У вас ' + str(
                     variables[chat_id]['tries']) + (' попыток' if variables[chat_id]['tries'] >= 5 else ' попытки' if
                 variables[chat_id]['tries'] > 1 else ' попытка')), reply_markup=None)
             bot.register_next_step_handler(msg, guessStep)
